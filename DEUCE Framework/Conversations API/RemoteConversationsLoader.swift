db34d31fb3a130545b6bf709f610db5ba7d2dffd
Jose Alvarez <jalvarez1@costar.com>
2019-05-07 16:25:50 -0700

    add RemoteConversationsLoader

---
+++ DEUCE Framework/Conversations API/RemoteConversationsLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteConversationsLoader.swift
+//  DEUCE Framework
+//
+//  Created by Jose Alvarez on 5/7/19.
+//  Copyright © 2019 DEUCE. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteConversationsLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
