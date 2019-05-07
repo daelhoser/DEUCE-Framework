ed0b5e0363f32149997ef9aa48944fd1ae15bfd6
Jose Alvarez <jalvarez1@costar.com>
2019-05-07 16:30:40 -0700

    Add RemoteConversations Test Methods.

--- DEUCE FrameworkTests/RemoteConversationsLoaderTests.swift
+++ DEUCE FrameworkTests/RemoteConversationsLoaderTests.swift
@@ -7,27 +7,47 @@
 //
 
 import XCTest
+import DEUCE_Framework
 
 class RemoteConversationsLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteConversationsLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteConversationsLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+
+        func get(from url: URL) {
+            requestedURLs.append(url)
         }
     }
-
 }
