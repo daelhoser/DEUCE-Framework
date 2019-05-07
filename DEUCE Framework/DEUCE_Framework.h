6dadd14e7bf07366c25c22b31d583800010c448b
Jose Alvarez <jalvarez1@costar.com>
2019-05-07 16:18:24 -0700

    Initial Commit

---
+++ DEUCE Framework/DEUCE_Framework.h
@@ -0,0 +1,19 @@
+//
+//  DEUCE_Framework.h
+//  DEUCE Framework
+//
+//  Created by Jose Alvarez on 5/7/19.
+//  Copyright © 2019 DEUCE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DEUCE_Framework.
+FOUNDATION_EXPORT double DEUCE_FrameworkVersionNumber;
+
+//! Project version string for DEUCE_Framework.
+FOUNDATION_EXPORT const unsigned char DEUCE_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DEUCE_Framework/PublicHeader.h>
+
+
