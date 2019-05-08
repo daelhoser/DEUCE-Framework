33f466ef950f639c93eb3675841b6f96e2015ec6
Jose Alvarez <jalvarez1@costar.com>
2019-05-07 17:07:05 -0700

    delivers connectivity error on client error.

--- DEUCE FrameworkTests/RemoteConversationsLoaderTests.swift
+++ DEUCE FrameworkTests/RemoteConversationsLoaderTests.swift
@@ -35,6 +35,16 @@ class RemoteConversationsLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0, userInfo: nil)
+
+        var capturedError: RemoteConversationsLoader.Error?
+        sut.load { error in capturedError = error }
+
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteConversationsLoader, client: HTTPClientSpy) {
@@ -45,9 +55,13 @@ class RemoteConversationsLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestedURLs.append(url)
+            if let error  = error {
+                completion(error)
+            }
         }
     }
 }
