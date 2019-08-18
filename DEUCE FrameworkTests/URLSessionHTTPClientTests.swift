395447856ed41b4053a1955be26562e637c4bd2d
Jose Alvarez <jalvarez1@costar.com>
2019-08-18 11:54:45 -0700

    Removed redundant tests.

--- DEUCE FrameworkTests/URLSessionHTTPClientTests.swift
+++ DEUCE FrameworkTests/URLSessionHTTPClientTests.swift
@@ -21,16 +21,6 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-
-        sut.get(from: url)
-
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -48,27 +38,17 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
 
     private class URLSessionSpy: URLSession {
-        var receivedURLs = [URL]()
         private var stubs = [URL: URLSessionDataTask]()
 
-        private struct Stub {
-            var receivedURL: URL
-            var task: URLSessionDataTask
-        }
-
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
-
-            return stubs[url] ?? FakeURLSessionDataTask()
+            guard let task = stubs[url] else {
+                fatalError("Expected stub. Received no stub instead")
+            }
+            return task
         }
 
         func stub(url: URL, with task: URLSessionDataTask) {
             stubs[url] = task
-        }
-    }
-
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {
         }
     }
 
