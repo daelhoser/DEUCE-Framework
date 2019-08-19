9ea6bb8979019279da8c0d7cbc37c6ffeb0ae3e9
Jose Alvarez <jalvarez1@costar.com>
2019-08-19 10:13:56 -0700

    move start and stop intercepting request to setup and teardown methods.

--- DEUCE FrameworkTests/URLSessionHTTPClientTests.swift
+++ DEUCE FrameworkTests/URLSessionHTTPClientTests.swift
@@ -26,11 +26,19 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_performGETRequestWithURL() {
+
+    override func setUp() {
         URLProtocolStub.startInterceptingRequest()
+    }
+
+    override func tearDown() {
+        URLProtocolStub.stopInterceptingRequest()
+    }
+
+    func test_getFromURL_performGETRequestWithURL() {
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
-        
+
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
@@ -41,10 +49,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequest()
     }
+
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequest()
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 0)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
@@ -65,7 +72,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequest()
     }
 
     // MARK: - Helpers
