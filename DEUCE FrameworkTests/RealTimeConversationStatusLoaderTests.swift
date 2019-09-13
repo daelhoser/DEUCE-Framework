97ac188343ae4be3e430aabb3b0e315a0d06b9e4
Jose Alvarez <jalvarez1@costar.com>
2019-09-13 13:55:14 -0700

    1.create makeSUT helper method. 2. returns error on client error

--- DEUCE FrameworkTests/RealTimeConversationStatusLoaderTests.swift
+++ DEUCE FrameworkTests/RealTimeConversationStatusLoaderTests.swift
@@ -15,34 +15,67 @@ final class RealTimeConversationStatusLoader {
         self.client = client
     }
 
-    func connect() {
-        client.connect()
+    func connect(completion: @escaping (Error) -> Void) {
+        client.connect(completion: completion)
     }
 }
 
 class RealTimeConversationStatusLoaderTests: XCTestCase {
     func test_onInit_doesNotAttemptConnectToClient() {
-        let client = RealTimeClientSpy()
-        _ = RealTimeConversationStatusLoader(client: client)
+        let (client, _) = makeSUT()
 
         XCTAssertFalse(client.attemptedConnections)
     }
 
     func test_onConnect_attemptsToMakeAConnection() {
+        let (client, loader) = makeSUT()
+
+        loader.connect { _ in }
+
+        XCTAssertTrue(client.attemptedConnections)
+    }
+
+    func test_onConnect_notifiesConnectionErrorOnError() {
+        let (client, loader) = makeSUT()
+
+        var receivedError: Error!
+        let exp = expectation(description: "wait on connect")
+
+        loader.connect { error in
+            receivedError = error
+            exp.fulfill()
+        }
+
+        let clientError = NSError(domain: "any error", code: 0)
+        client.completesWithError(clientError)
+
+        wait(for: [exp], timeout: 1.0)
+
+        XCTAssertEqual(clientError, receivedError as NSError)
+    }
+
+    private func makeSUT() -> (RealTimeClientSpy, RealTimeConversationStatusLoader) {
         let client = RealTimeClientSpy()
         let loader = RealTimeConversationStatusLoader(client: client)
 
-        loader.connect()
+        trackForMemoryLeaks(object: client)
+        trackForMemoryLeaks(object: loader)
 
-        XCTAssertTrue(client.attemptedConnections)
+        return (client, loader)
     }
 }
 
 class RealTimeClientSpy {
     private(set) var attemptedConnections = false
+    private var completions = [(Error) -> Void]()
 
-    func connect() {
+    func connect(completion: @escaping (Error) -> Void) {
         attemptedConnections = true
+        completions.append(completion)
+    }
+
+    func completesWithError(_ error: NSError, at index: Int = 0) {
+        completions[index](error)
     }
 }
 
