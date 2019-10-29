2429ce442db7f03303ddd0677be69bd9fd7d8afb
Jose Alvarez <jalvarez1@costar.com>
2019-10-29 10:11:26 -0700

    on load, loader requests data

--- DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
+++ DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
@@ -9,21 +9,44 @@
 import XCTest
 
 class ConversationUsersLoader {
-    init(client: ClientSpy) {
+    let url: URL
+    let client: ClientSpy
 
+    init(url: URL, client: ClientSpy) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.load(url: url)
     }
 }
 
 class RemoteConversationUsersLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://a-url.com")!
         let client = ClientSpy()
-        _ = ConversationUsersLoader(client: client)
+        _ = ConversationUsersLoader(url: url, client: client)
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "http://a-url.com")!
+        let client = ClientSpy()
+        let loader = ConversationUsersLoader(url: url, client: client)
+
+        loader.load()
+
+        XCTAssertEqual(client.requestedURLs.count, 1)
     }
 }
 
 class ClientSpy {
     private(set) var requestedURLs = [URL]()
+
+    func load(url: URL) {
+        requestedURLs.append(url)
+    }
 }
 
