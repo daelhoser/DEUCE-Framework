579c0c6d6f444668a21c78bbfc1537d8a51ee958
Jose Alvarez <jalvarez1@costar.com>
2019-10-30 20:57:59 -0700

    deliver an invalidData error on non 200 HTTPUrlResponse.

--- DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
+++ DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
@@ -23,8 +23,12 @@ class ConversationUsersLoader {
     }
 
     func load(completion: @escaping (Error) -> Void) {
-        client.load(url: url) { (error) in
-            completion(.connection)
+        client.load(url: url) { (response, error) in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connection)
+            }
         }
     }
 }
@@ -73,21 +77,26 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-//    func test_load_deliversErrorOnNon200HttpResponse() {
-//        let (clients, loader) = makeSUT()
-//
-//        let samples = [199, 201, 300, 400, 500]
-//
-//        var capturedErro: Error?
-//
-//
-//
-//        samples.enumerated().forEach { (index, sample) in
-//            loader.load { (error) in
-//                capturedError = error
-//            }
-//        }
-//    }
+    func test_load_deliversErrorOnNon200HttpResponse() {
+        let (client, loader) = makeSUT()
+
+        let samples = [199, 201, 300, 400, 500]
+
+        let exp = expectation(description: "waiting")
+        exp.expectedFulfillmentCount = samples.count
+
+        samples.enumerated().forEach { (index, sample) in
+            loader.load { (error) in
+                var capturedError = [ConversationUsersLoader.Error]()
+                capturedError.append(error)
+                XCTAssertEqual(capturedError, [.invalidData])
+
+                exp.fulfill()
+            }
+            client.compleWithStatusCodeError(statusCode: sample)
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
 
 
 
@@ -106,18 +115,24 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
 }
 
 class ClientSpy {
-    private(set) var requests = [(url: URL, completion: ((Error) -> Void)?)]()
+    private(set) var requests = [(url: URL, completion: ((HTTPURLResponse?, Error?) -> Void)?)]()
 
     var requestedURLs: [URL] {
         return requests.map { $0.url }
     }
 
-    func load(url: URL, completion: @escaping (Error) -> Void) {
+    func load(url: URL, completion: @escaping (HTTPURLResponse?, Error?) -> Void) {
         self.requests.append((url, completion))
     }
 
     func completeWith(error: Error) {
-        requests[0].completion?(error)
+        requests[0].completion?(nil, error)
+    }
+
+    func compleWithStatusCodeError(statusCode: Int) {
+        let url = requests[0].url
+        let response = HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil)
+        requests[0].completion?(response, nil)
     }
 }
 
