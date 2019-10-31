7de3708fb692d418416e4bbc2bae266449bab211
Jose Alvarez <jalvarez1@costar.com>
2019-10-31 09:55:42 -0700

    Allow loader to pass in Result type instead of error as part of its response.

--- DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
+++ DEUCE FrameworkTests/Create Conversation API/RemoteConversationUsersLoaderTests.swift
@@ -9,6 +9,9 @@
 import XCTest
 import DEUCE_Framework
 
+struct ConversationUser: Equatable {
+}
+
 class ConversationUsersLoader {
     let url: URL
     let client: HTTPClient
@@ -19,22 +22,29 @@ class ConversationUsersLoader {
         case unauthorized
     }
 
+    enum LoadConversationUsersResult {
+        case success([ConversationUser])
+        case failure(Error)
+    }
+
+    typealias Result = LoadConversationUsersResult
+
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
 
-    func load(completion: @escaping (Error) -> Void) {
+    func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { (result) in
             switch result {
             case let .success(_, urlResponse):
                 if urlResponse.statusCode == 401 {
-                    return completion(.unauthorized)
+                    return completion(.failure(.unauthorized))
                 } else {
-                    return completion(.invalidData)
+                    return completion(.failure(.invalidData))
                 }
             case .failure:
-                return completion(.connection)
+                return completion(.failure(.connection))
             }
         }
     }
@@ -68,7 +78,7 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
         let (client, loader) = makeSUT()
         let clientError = NSError(domain: "any-error", code: 0)
 
-        expect(sut: loader, toCompleteWith: .connection, when: {
+        expect(sut: loader, toCompleteWith: failure(.connection), when: {
             client.completeWith(error: clientError)
         })
     }
@@ -79,7 +89,7 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
 
         samples.enumerated().forEach { (index, sample) in
-            expect(sut: loader, toCompleteWith: .invalidData, when: {
+            expect(sut: loader, toCompleteWith: failure(.invalidData), when: {
                 client.completeWith(statusCode: sample, data: "any-data".data(using: .utf8)!)
             })
         }
@@ -89,7 +99,7 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
         let (client, loader) = makeSUT()
         let unauthorizedStatusCode = 401
 
-        expect(sut: loader, toCompleteWith: .unauthorized, when: {
+        expect(sut: loader, toCompleteWith: failure(.unauthorized), when: {
             client.completeWith(statusCode: unauthorizedStatusCode, data: "any-data".data(using: .utf8)!)
         })
     }
@@ -97,12 +107,20 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (client, loader) = makeSUT()
 
-        expect(sut: loader, toCompleteWith: .invalidData, when: {
+        expect(sut: loader, toCompleteWith: failure(.invalidData), when: {
             let invalidData = "invalid-Data".data(using: .utf8)!
             client.completeWith(statusCode: 200, data: invalidData)
         })
     }
 
+//    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+//        let (client, loader) = makeSUT()
+//
+//        expect(sut: loader, toCompleteWith: .success([]), when: {
+//            let emptyArray = "[]".data(using: .utf8)!
+//            client.completeWith(statusCode: 200, data: emptyArray)
+//        })
+//    }
 
     // MARK: - Helper Methods
 
@@ -117,17 +135,28 @@ class RemoteConversationUsersLoaderTests: XCTestCase {
         return (client, loader)
     }
 
-    private func expect(sut: ConversationUsersLoader, toCompleteWith error:ConversationUsersLoader.Error, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    private func expect(sut: ConversationUsersLoader, toCompleteWith result: ConversationUsersLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Waiting on load")
 
-        sut.load { (receivedError) in
-            XCTAssertEqual(error, receivedError, "Expected error \(error), received \(receivedError) instead", file: file, line: line)
+        sut.load { (receivedResult) in
+            switch (result, receivedResult) {
+            case let (.failure(error), .failure(receivedError)):
+                XCTAssertEqual(error, receivedError, "Expected error \(error), received \(receivedError) instead", file: file, line: line)
+            case let (.success(items), .success(receivedItems)):
+                XCTAssertEqual(items, receivedItems, "Expected items \(items), received \(receivedItems) instead", file: file, line: line)
+            default:
+                XCTFail("Expected result \(result), received \(receivedResult) instead", file: file, line: line)
+            }
         }
         exp.fulfill()
 
         action()
 
         wait(for: [exp], timeout: 1.0)
+    }
+
+    private func failure(_ error: ConversationUsersLoader.Error) -> ConversationUsersLoader.Result {
+        return .failure(error)
     }
 }
 
