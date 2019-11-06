2fdd87512487082605a8d2c0c4e91d8a038a28c0
Jose Alvarez <jalvarez1@costar.com>
2019-11-06 10:35:35 -0800

    Created ConversationUserMapper class to perform the SRP

--- DEUCE Framework/Conversation Users/Conversation Users API/ConversationUsersLoader.swift
+++ DEUCE Framework/Conversation Users/Conversation Users API/ConversationUsersLoader.swift
@@ -34,53 +34,11 @@ public class ConversationUsersLoader {
         client.get(from: url) { (result) in
             switch result {
             case let .success(data, urlResponse):
-                return completion(ConversationUsersLoader.map(response: urlResponse, data: data))
+                return completion(ConversationUsersMapper.map(response: urlResponse, data: data))
             case .failure:
                 // did not reach server
                 return completion(.failure(.connection))
             }
         }
-    }
-
-    private static func map(response: HTTPURLResponse, data: Data) -> Result {
-        if response.statusCode == 401 {
-            return .failure(.unauthorized)
-        } else if response.statusCode == 200 {
-            let jsonDecoder = JSONDecoder()
-
-            guard let payload = try? jsonDecoder.decode(ConversationUserStatusData.self, from: data) else {
-                return .failure(.invalidData)
-            }
-
-            let users = payload.users.map { return $0.user }
-
-            return .success(users)
-        }
-        return .failure(.invalidData)
-    }
-
-}
-
-private struct ConversationUserStatusData: Decodable {
-    let users: [ConvoUser]
-
-    private enum CodingKeys: String, CodingKey {
-        case users = "payload"
-    }
-}
-
-private struct ConvoUser: Decodable {
-    private let id: String
-    private let displayName: String
-    private var thumbnailURL: URL?
-
-    private enum CodingKeys: String, CodingKey {
-        case id = "Id"
-        case displayName = "DisplayName"
-        case thumbnailURL = "ThumbnailUrl"
-    }
-
-    var user: ConversationUser {
-        return ConversationUser(id: id, displayName: displayName, thumbnailURL: thumbnailURL)
     }
 }
