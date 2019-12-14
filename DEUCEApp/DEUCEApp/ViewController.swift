05f5ba0d59a183a281f00a1e482414454ceeb73f
Jose <daelhoser@gmail.com>
2019-12-13 17:50:18 -0800

    Add empty DEUCEApp Project.

---
+++ DEUCEApp/DEUCEApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DEUCEApp
+//
+//  Created by Jose Alvarez on 12/13/19.
+//  Copyright © 2019 DEUCE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
