0906815784b4752d806d1ebf46cf8e517974ef2c
Jose <daelhoser@gmail.com>
2019-12-14 20:35:50 -0800

    image loader does not request on init.

--- DEUCE FrameworkTests/API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ DEUCE FrameworkTests/API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -8,5 +8,29 @@
 
 import XCTest
 
+class RemoteFeedImageDataLoader {
+    init(client: Any) {
+        
+    }
+}
+
 class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
+    func test_onInit_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    // MARK: - Helper Methods
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(object: sut, file: file, line: line)
+        trackForMemoryLeaks(object: client, file: file, line: line)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy {
+        var requestedURLs = [URL]()
+    }
 }
