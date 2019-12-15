f48e563df13948437d1a152304dbed2bc0f49aaf
Jose <daelhoser@gmail.com>
2019-12-14 20:27:47 -0800

    Create SharedTestHelper file to put reusable code.

--- DEUCE FrameworkTests/API/URLSessionHTTPClientTests.swift
+++ DEUCE FrameworkTests/API/URLSessionHTTPClientTests.swift
@@ -198,11 +198,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         return receivedResult
     }
 
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-
-    }
-
     private func anyData() -> Data {
         return "any data".data(using: .utf8)!
     }
