303eef87be89f035743579d3aea277fcd87142ba
Jose <daelhoser@gmail.com>
2019-12-14 20:13:53 -0800

    create LoadFeedImageDataFromRemoteUseCaseTests test file.

---
+++ DEUCE FrameworkTests/API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  LoadFeedImageDataFromRemoteUseCaseTests.swift
+//  DEUCE FrameworkTests
+//
+//  Created by Jose Alvarez on 12/14/19.
+//  Copyright © 2019 DEUCE. All rights reserved.
+//
+
+import XCTest
+
+class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
+}
