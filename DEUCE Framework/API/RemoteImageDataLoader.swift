3b7a2761318e57309996ae643836dbbf2bfa4ccf
Jose <daelhoser@gmail.com>
2019-12-16 12:32:18 -0800

    set RemoteImageDataLoader to conform to ImageDataLoader.

--- DEUCE Framework/API/RemoteImageDataLoader.swift
+++ DEUCE Framework/API/RemoteImageDataLoader.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public final class RemoteImageDataLoader {
+public final class RemoteImageDataLoader: ImageDataLoader {
     private let client: HTTPClient
     
     public init(client: HTTPClient) {
