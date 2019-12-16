5af1953054776f36ecbd88a5d89107b039cc6e7d
Jose <daelhoser@gmail.com>
2019-12-16 09:16:27 -0800

    1. Reorganize to have domain specific setup. 2. Moved the RemoteImageLoader into its file.

--- DEUCE FrameworkTests/API/LoadImageDataFromRemoteUseCaseTests.swift
+++ DEUCE FrameworkTests/API/LoadImageDataFromRemoteUseCaseTests.swift
@@ -9,58 +9,6 @@
 import XCTest
 import DEUCE_Framework
 
-class RemoteImageDataLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private final class HTTPClientTaskWrapper: ImageDataLoaderTask {
-        private var completion: ((ImageDataLoader.Result) -> Void)?
-        
-        var wrapped: HTTPClientTask?
-        
-        init(_ completion: @escaping (ImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: ImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    public func loadImageData(from url: URL, completion: @escaping (ImageDataLoader.Result) -> Void) -> ImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-
-            switch result {
-            case .failure:
-                task.complete(with: .failure(Error.connectivity))
-            case let .success(data, response):
-                let isValidResponse = response.statusCode == 200 && !data.isEmpty
-                isValidResponse ? task.complete(with: .success(data)) : task.complete(with: .failure(Error.invalidData))
-            }
-        }
-        return task
-    }
-}
-
 class LoadImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_onInit_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
