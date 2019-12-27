c9c20e166a6b6141b997fdc4b0306368ce6ac56a
Jose <daelhoser@gmail.com>
2019-12-26 16:19:04 -0800

    Create RealTimeController and its dependencies.

---
+++ RealTimeController/RealTimeControllerTests/RealTimeControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  RealTimeControllerTests.swift
+//  RealTimeControllerTests
+//
+//  Created by Jose Alvarez on 12/26/19.
+//  Copyright © 2019 DEUCE. All rights reserved.
+//
+
+import XCTest
+
+@testable import RealTimeController
+
+
+
+class RealTimeControllerTests: XCTestCase {
+}
