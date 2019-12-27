49ce4d51624ecc91122c90154fb84372e68ba7c2
Jose <daelhoser@gmail.com>
2019-12-26 16:23:52 -0800

    On init does not begin connection.

---
+++ RealTimeController/Pods/Target Support Files/Pods-RealTimeController-RealTimeControllerTests/Pods-RealTimeController-RealTimeControllerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealTimeController_RealTimeControllerTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealTimeController_RealTimeControllerTests
+@end
--- RealTimeController/RealTimeControllerTests/RealTimeControllerTests.swift
+++ RealTimeController/RealTimeControllerTests/RealTimeControllerTests.swift
@@ -8,9 +8,26 @@
 
 import XCTest
 
-@testable import RealTimeController
+import RealTimeController
 
-
+class RealTimeController {
+    let client: ClientSpy
+    
+    init(client: ClientSpy) {
+        self.client = client
+    }
+}
 
 class RealTimeControllerTests: XCTestCase {
+    func test_onInit_doesNotBeginConnection() {
+        let client = ClientSpy()
+        _ = RealTimeController(client: client)
+        
+        XCTAssertEqual(client.connectionRequests, 0)
+    }
 }
+
+class ClientSpy {
+    var connectionRequests = 0
+}
+
