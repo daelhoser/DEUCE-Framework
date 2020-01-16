8689044766711903c24abe680ba8afcf302aaa06
Jose <daelhoser@gmail.com>
2020-01-16 15:06:02 -0800

    on connect return connected

--- RealTimeController/RealTimeControllerTests/RealTimeControllerTests.swift
+++ RealTimeController/RealTimeControllerTests/RealTimeControllerTests.swift
@@ -10,32 +10,82 @@ import XCTest
 
 import RealTimeController
 import SignalRSwift
+import DEUCE_Framework
 
 protocol RealTimeProxy {
 }
 
 protocol RealTimeConnection {
+    func start()
+    var started: (() -> Void)? { set get }
 }
 
-final class RealTimeClient {
+final class SignalRClient: RealTimeClient  {
     let proxy: RealTimeProxy
-    let connection: RealTimeConnection
+    var connection: RealTimeConnection
     
     init(proxy: RealTimeProxy, connection: RealTimeConnection) {
         self.proxy = proxy
         self.connection = connection
+    }
+    
+    func connectTo(url: URL, result: @escaping (RealTimeClientResult) -> Void) {
+        connection.started = {
+            result(.connected)
+        }
+
+        connection.start()
     }
 }
 
 class RealTimeControllerTests: XCTestCase {
     func test_onInit_doesNotConnect() {
         let spy = RealTimeSpy()
-        _ = RealTimeClient(proxy: spy, connection: spy)
+        _ = SignalRClient(proxy: spy, connection: spy)
         
         XCTAssertEqual(spy.connectionRequests, 0)
+    }
+    
+    func test_onConnect_ReturnsConnectedResultWhenConnectionSuccessful() {
+        let spy = RealTimeSpy()
+        let realTimeClient = SignalRClient(proxy: spy, connection: spy)
+                
+        var capturedResult: RealTimeClientResult?
+        
+        let exp = expectation(description: "Waiting to connect")
+        
+        realTimeClient.connectTo(url: URL(string: "www.google.com")!) { (result) in
+            capturedResult = result
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(capturedResult, RealTimeClientResult.connected)
     }
 }
 
 class RealTimeSpy: RealTimeProxy, RealTimeConnection {
     private(set) var connectionRequests = 0
+    
+    // MARK: - RealTimeConnection
+    
+    var started: (() -> Void)?
+
+    func start() {
+        started?()
+    }
+}
+
+
+extension RealTimeClientResult: Equatable {
+    public static func == (lhs: RealTimeClientResult, rhs: RealTimeClientResult) -> Bool {
+        switch (lhs, rhs) {
+        case (.connected, .connected):
+            return true
+        default:
+            return false
+        }
+    }
 }
